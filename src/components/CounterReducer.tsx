4377ccdb912186db258cf214ad942619810b2391
pixelCode4 <155477364+pixelCode4@users.noreply.github.com>
2024-04-08 15:21:52 -0300

    useReducer

--- src/components/CounterReducer.tsx
+++ src/components/CounterReducer.tsx
@@ -1,32 +1,60 @@
-
+import { useReducer } from "react";
 
 const initialState = {
   counter: 0,
-}
+};
 
-type ActionType = {
-  type: 'increment'
-}
+type ActionType =
+  | { type: "increment" }
+  | { type: "decrement" }
+  | { type: "custom"; payload: number };
 
 const counterReducer = (state: typeof initialState, action: ActionType) => {
-switch (action.type) {
-  case 'increment':
-    return{
-      ...state,
-      counter: state.counter
-    }
-    
-    break;
+  switch (action.type) {
+    case "increment":
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+    case "decrement":
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    case "custom":
+      return {
+        ...state,
+        counter: state.counter + action.payload,
+      };
 
-  default:
-    break;
-}
-}
+    default:
+      return state;
+  }
+};
 
 export const CounterReducer = () => {
+  const [counterState, dispatch] = useReducer(counterReducer, initialState);
   return (
     <>
-      <h2>Contador: {10}</h2>
+      <h2>Contador: {counterState.counter}</h2>
+      <button
+        className="btn btn-outline-primary"
+        onClick={() => dispatch({ type: "increment" })}
+      >
+        +1
+      </button>
+      <button
+        className="btn btn-outline-primary"
+        onClick={() => dispatch({ type: "decrement" })}
+      >
+        -1
+      </button>
+      <button
+        className="btn btn-outline-danger"
+        onClick={() => dispatch({ type: "custom", payload: 20 })}
+      >
+        +20
+      </button>
     </>
   );
 };
